6b18dfc8687e15078360cb6016b9e7e5c677f84f
alaidani <alaidani@gmail.org>
2026-02-24 09:28:21 -0500

    Commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double balance = 5000.00;
+        double apr = 0.17; // 17% annual rate
+        double monthlyRate = apr / 12.0; // monthly interest rate
+
+// After 1 month
+        double interestMonth1 = balance * monthlyRate;
+        double balanceAfter1 = balance + interestMonth1;
+
+// After 2 months (no payments)
+        double interestMonth2 = balanceAfter1 * monthlyRate;
+        double balanceAfter2 = balanceAfter1 + interestMonth2;
+
+
+
+        System.out.println("The interest for the first month is " + interestMonth1);
+        System.out.println("The interest for the second month is " + interestMonth2);
+    }
+}
